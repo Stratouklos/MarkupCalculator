023b21f7deb38b6689c764d347f7862d531d2827
Stratos <stratouklos@gmail.com>
2013-12-20 02:53:54 +0200

    Adding a high level initial solution sketch

---
+++ src/test/java/com/nullpointerengineering/MainTest.java
@@ -0,0 +1,26 @@
+package com.nullpointerengineering;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stratos
+ * Test the main workflow.
+ */
+public class MainTest {
+
+    @Test
+    @Ignore
+    public void mainTest() {
+        /*
+        String input = "Some input"
+        OrderCollectionBuilder orderBuilder = new OrderCollectionBuilderFromFile(input)
+        Collection<Order> orders = orderBuilder.build()
+        MarkupCalculator calculator = new MarkupCalculator(orders);
+        String output = calculator.calculateMarkup();
+        System.out.print(output)
+         */
+    }
+
+}

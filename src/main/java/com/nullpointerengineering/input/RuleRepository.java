f343e79b4045d8b374fa640c10d4819c752a62ca
Stratos <stratouklos@gmail.com>
2014-01-03 17:21:06 +0200

    Committing omitted file

---
+++ src/main/java/com/nullpointerengineering/input/RuleRepository.java
@@ -0,0 +1,13 @@
+package com.nullpointerengineering.input;
+
+import java.util.Collection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stratos
+ * Hold the business rules
+ */
+public interface RuleRepository {
+
+    Collection<?> getRules();
+}

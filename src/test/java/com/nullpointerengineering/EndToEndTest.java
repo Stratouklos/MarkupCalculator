3528be6247cadfc00cb2f7dc81e0553466857c72
Stratos <stratouklos@gmail.com>
2014-01-11 23:34:04 -0500

    End to end test fails for the moment

---
+++ src/test/java/com/nullpointerengineering/EndToEndTest.java
@@ -0,0 +1,49 @@
+package com.nullpointerengineering;
+
+import com.nullpointerengineering.data.OrderedRuleRepository;
+import com.nullpointerengineering.data.RuleRepository;
+import com.nullpointerengineering.input.LineReaderFromFile;
+import com.nullpointerengineering.input.RuleParser;
+import com.nullpointerengineering.input.ThreeLineOrderParser;
+import com.nullpointerengineering.model.*;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stratos
+ * Test the main workflow.
+ */
+public class EndToEndTest {
+    @Test
+    @Ignore
+    public void singleOrder() throws IOException {
+        LineReaderFromFile orderReader = new LineReaderFromFile("C:\\Users\\Stratos\\code\\markupCalculator\\src\\test\\resources\\one_order");
+        ThreeLineOrderParser orderParser = new ThreeLineOrderParser();
+        orderReader.read(orderParser);
+
+        Collection<Order> orders = orderParser.getOrders();
+        Order order = orders.iterator().next();
+
+        LineReaderFromFile ruleReader = new LineReaderFromFile("C:\\Users\\Stratos\\code\\markupCalculator\\src\\test\\resources\\five_rules");
+        FinancialRuleComparator ruleComparator = FinancialRuleComparator.first(FlatMarkupRule.class);
+        RuleRepository ruleRepository =  new OrderedRuleRepository(ruleComparator);
+        RuleParser ruleParser = new RuleParser(new FinancialRuleFactory(), ruleRepository);
+        ruleReader.read(ruleParser);
+        Collection<FinancialRule> rules = ruleRepository.getRules();
+
+        ValueCalculator calculator = new ValueCalculator(rules);
+
+        BigDecimal totalValue = calculator.calculateTotalValue(order);
+
+        assertThat(totalValue, is(BigDecimal.valueOf(1591.58)));
+    }
+}

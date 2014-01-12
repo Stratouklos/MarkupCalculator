5677cb600a98ee97598a09439c4f23c7536ad6f4
Stratos <stratouklos@gmail.com>
2014-01-12 11:20:39 -0500

    Adding second e2e test

--- src/test/java/com/nullpointerengineering/EndToEndTest.java
+++ src/test/java/com/nullpointerengineering/EndToEndTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.Iterator;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -44,4 +45,30 @@ public class EndToEndTest {
 
         assertThat(totalValue, is(BigDecimal.valueOf(1591.58)));
     }
+
+    @Test
+    public void twoOrders() throws IOException {
+        LineReaderFromFile orderReader = new LineReaderFromFile("C:\\Users\\Stratos\\code\\markupCalculator\\src\\test\\resources\\two_orders");
+        ThreeLineOrderParser orderParser = new ThreeLineOrderParser();
+        orderReader.read(orderParser);
+        LineReaderFromFile ruleReader = new LineReaderFromFile("C:\\Users\\Stratos\\code\\markupCalculator\\src\\test\\resources\\five_rules");
+        FinancialRuleComparator ruleComparator = FinancialRuleComparator.first(FlatMarkupRule.class);
+        RuleRepository ruleRepository =  new OrderedRuleRepository(ruleComparator);
+        RuleParser ruleParser = new RuleParser(new FinancialRuleFactory(), ruleRepository);
+        ruleReader.read(ruleParser);
+        Collection<FinancialRule> rules = ruleRepository.getRules();
+        ValueCalculator calculator = new ValueCalculator(rules);
+
+        Iterator<Order> orders = orderParser.getOrders().iterator();
+
+        Order order1 = orders.next();
+        Order order2 = orders.next();
+
+        BigDecimal totalValue1 = calculator.calculateTotalValue(order1);
+        assertThat(totalValue1, is(BigDecimal.valueOf(6199.81)));
+
+        BigDecimal totalValue2 = calculator.calculateTotalValue(order2);
+        assertThat(totalValue2, is(BigDecimal.valueOf(1591.58)));
+    }
+
 }

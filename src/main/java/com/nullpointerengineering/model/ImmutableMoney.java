8a1124c6014fd87347be87f153f4dbc92c9e48b6
Stratos <stratouklos@gmail.com>
2014-01-18 01:08:18 -0500

    Adding new Money type

---
+++ src/main/java/com/nullpointerengineering/model/ImmutableMoney.java
@@ -0,0 +1,56 @@
+package com.nullpointerengineering.model;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static java.math.BigDecimal.valueOf;
+import static java.math.RoundingMode.HALF_EVEN;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stratos
+ */
+public class ImmutableMoney implements Money{
+
+    private static final BigDecimal ONE_HUNDRED = valueOf(100);
+    private static final int SCALE = 2;
+
+    private final BigDecimal value;
+    private final Currency currency = Currency.getInstance("USD");
+
+    public ImmutableMoney(String valueString) {
+        this(new BigDecimal(valueString));
+    }
+
+    public ImmutableMoney(BigDecimal value) {
+        this.value = value.setScale(SCALE, HALF_EVEN);
+    }
+
+    @Override
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    @Override
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public Money applyPercentage(BigDecimal percentage) {
+        return new ImmutableMoney(value.multiply(percentage).divide(ONE_HUNDRED));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("$%(.2f", value);
+    }
+
+    @Override
+    public Money add(Money moreMoney) {
+        if (!this.currency.equals(moreMoney.getCurrency()))
+            throw new IllegalArgumentException("Currency mismatch convert the currency first");
+        return new ImmutableMoney(moreMoney.getValue().add(value));
+    }
+
+}
---
+++ src/test/java/com/nullpointerengineering/model/MoneyTest.java
@@ -0,0 +1,111 @@
+package com.nullpointerengineering.model;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Currency;
+
+import static java.math.BigDecimal.valueOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stratos
+ */
+@RunWith(JUnit4.class)
+public class MoneyTest {
+
+    Money moneyUnderTest;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void stringConstructorTest() {
+        moneyUnderTest = new ImmutableMoney("10");
+        assertThat(moneyUnderTest.getValue(), is(valueOf(10).setScale(2)));
+    }
+
+
+    @Test
+    public void negativeStringConstructorTest() {
+        moneyUnderTest = new ImmutableMoney("-10");
+        assertThat(moneyUnderTest.getValue(), is(valueOf(-10).setScale(2)));
+    }
+
+    @Test
+    public void integerStringValueScaledProperly() {
+        moneyUnderTest = new ImmutableMoney("10");
+        assertThat(moneyUnderTest.getValue().scale(), is(2));
+    }
+
+    @Test
+    public void decimalStringValueScaledProperly() {
+        moneyUnderTest = new ImmutableMoney("10.666");
+        assertThat(moneyUnderTest.getValue().scale(), is(2));
+    }
+
+    @Test
+    public void bigDecimalValueScaledProperly() {
+        moneyUnderTest = new ImmutableMoney(valueOf(34.7665));
+        assertThat(moneyUnderTest.getValue().scale(), is(2));
+    }
+
+    @Test
+    public void addTest() {
+        moneyUnderTest = new ImmutableMoney("10");
+        Money expectedMoney = moneyUnderTest.add(new ImmutableMoney("5.25"));
+        assertThat(expectedMoney.toString(), is("$15.25"));
+    }
+
+    @Test
+    public void addNegativeTest() {
+        moneyUnderTest = new ImmutableMoney("10.76");
+        Money expectedMoney = moneyUnderTest.add(new ImmutableMoney("-25.25"));
+        assertThat(expectedMoney.toString(), is("$(14.49)"));
+    }
+
+    @Test
+    public void applyPercentageTest(){
+        moneyUnderTest = new ImmutableMoney("10");
+        Money expectedMoney = moneyUnderTest.applyPercentage(valueOf(10));
+        assertThat(expectedMoney.toString(), is("$1.00"));
+    }
+
+    @Test
+    public void applyNegativePercentageTest(){
+        moneyUnderTest = new ImmutableMoney("10");
+        Money expectedMoney = moneyUnderTest.applyPercentage(valueOf(-10));
+        assertThat(expectedMoney.toString(), is("$(1.00)"));
+    }
+
+    @Test
+    public void positiveToStringTest() {
+        moneyUnderTest = new ImmutableMoney(valueOf(3.14));
+        assertThat(moneyUnderTest.toString(), is("$3.14"));
+    }
+
+    @Test
+    public void negativeToStringTest() {
+        moneyUnderTest = new ImmutableMoney(valueOf(-3.14));
+        assertThat(moneyUnderTest.toString(), is("$(3.14)"));
+    }
+
+    @Test
+    public void currencyMismatch() {
+        moneyUnderTest = new ImmutableMoney(valueOf(3.14));
+        Money euro = mock(Money.class);
+        when(euro.getCurrency()).thenReturn(Currency.getInstance("EUR"));
+
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Currency mismatch");
+        moneyUnderTest.add(euro);
+    }
+
+}
